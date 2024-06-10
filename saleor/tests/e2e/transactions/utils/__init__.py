365f40bb478119b58cb16b0ce9e85880956d87d2
nurali <nmakhmutzhan04@gmail.com>
2024-06-10 07:09:51 +0500

    second

--- saleor/tests/e2e/transactions/utils/__init__.py
+++
@@ -1,7 +0,0 @@
-from .transaction_create import create_transaction
-from .transaction_initialize import transaction_initialize
-
-__all__ = [
-    "create_transaction",
-    "transaction_initialize",
-]
